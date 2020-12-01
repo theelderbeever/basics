185f8093b1aae67ce76be8dec3086751561d1674
Taylor Beever <taybeever@gmail.com>
2020-12-01 16:56:49 -0700

    Add pre-commit hooks

--- basics/data.py
+++ basics/data.py
@@ -1,7 +1,7 @@
 import dvc.api
+import pandas as pd
 
 with dvc.api.open(
-    "data/data.xml",
-    repo="https://github.com/theelderbeever/basics"
+    "data/data.xml", repo="https://github.com/theelderbeever/basics"
 ) as fd:
-    print(fd)
+    print(pd.read_csv(fd))
